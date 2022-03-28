9cbbd85ccfb304a49a31620e5c9121752020d4c1
sajjadalijaved <sajjadali11755@gmail.com>
2022-03-28 20:34:56 +0500

    post

---
+++ android/app/src/main/kotlin/com/example/arc_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.arc_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:arc_project/clipper.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ClipPath(
+          clipper: Myclip(),
+          child: Container(
+            height: 400,
+            width: 400,
+            color: Colors.green,
+          ),
+        ),
+      ),
+    );
+  }
+}
